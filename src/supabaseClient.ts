63477d9976acd8d8cd5b3dc209b33183c3e32c02
Hussainpatanwala <134602451+Hussainpatanwala@users.noreply.github.com>
2026-05-29 17:43:41 +0530

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -6,7 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 // ============================================================
 
 const SUPABASE_URL = 'https://ezuoudyftsbkobbfjxbx.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'PASTE_YOUR_FULL_PUBLISHABLE_KEY_HERE';
+const SUPABASE_PUBLISHABLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6dW91ZHlmdHNia29iYmZqeGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzI2MDIsImV4cCI6MjA5NTYwODYwMn0.GhUCnA7oiShpEAM2kCFX7vCJc-xxj64uyxmaKgfjpEQ';
 
 
 // ============================================================
